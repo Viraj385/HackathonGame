d37c150d1f254f60c52393923bdb709687b9f32d
Viraj Patel <virajpatel@Virajs-MacBook-Air.local>
2025-06-29 18:51:38 +1200

    Added a background image and basic player movement

--- Main.py
+++ Main.py
@@ -6,13 +6,23 @@ pygame.init()
 width, height = 960, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Twisted Wizards")
+background = pygame.image.load("background.png")
+background = pygame.transform.scale(background, (width, height))
+
 
 # Colors
-White = (255, 255, 255)
-Black = (0, 0, 0)
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
 
 clock = pygame.time.Clock()
 gameRunning = True
+
+# Player Basic Setup
+player_size = (50, 50)
+player = pygame.Rect(100, height - 100, *player_size)
+player_color = red
+player_speed = 5
 
 while gameRunning:
     # Event Handler
@@ -21,5 +31,17 @@ while gameRunning:
             gameRunning = False
 
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.x -= player_speed
+    if keys[pygame.K_RIGHT]:
+        player.x += player_speed
+
+    # Draw 
+    screen.blit(background, (0, 0))
+    pygame.draw.rect(screen, player_color, player)
+
+    pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
+sys.exit()
